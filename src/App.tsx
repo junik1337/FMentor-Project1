d55748b1ae7a466b2fbe256fd6c3c1132c8d0210
Abderrachid Yassir <unico37@192.168.1.8>
2025-07-28 20:33:02 +0100

    Last Changes

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+function App() {
+  return (
+    <main className="flex h-[98%] flex-grow w-screen max-w-[1440px] flex-col items-center justify-center">
+      <div className="flex items-center justify-center flex-col max-w-xs gap-6 bg-white px-4 pt-4 pb-10 rounded-[20px]">
+        <img
+          src="/src/assets/image-qr-code.png"
+          alt="QR Code"
+          className="size-72 rounded-[10px]"
+        />
+
+        <div className="flex flex-col gap-4 px-4">
+          <h1
+            style={{
+              fontSize: "22px",
+              lineHeight: "120%",
+              letterSpacing: "0px",
+            }}
+            className="text-slate-900 font-bold text-center"
+          >
+            Improve your front-end skills by building projects
+          </h1>
+          <p
+            style={{
+              fontSize: "15px",
+              lineHeight: "140%",
+              letterSpacing: "0.2px",
+            }}
+            className="text-xs text-slate-500 text-center"
+          >
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Abderrachid Yassir</a>.
+    </div>
+  );
+};
+
+export default Footer;
