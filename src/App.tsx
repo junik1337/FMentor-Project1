7a6881d4e4280dffd377e0d1e5fbaaf32ffec244
Abderrachid Yassir <unico37@192.168.1.8>
2025-07-29 11:01:47 +0100

    Refactor: README changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <main className="flex h-[98%] flex-grow w-screen max-w-[1440px] flex-col items-center justify-center">
+    <main className="flex h-full flex-grow w-screen max-w-[1440px] flex-col items-center justify-center">
       <div className="flex items-center justify-center flex-col max-w-xs gap-6 bg-white px-4 pt-4 pb-10 rounded-[20px]">
         <img
           src="/image-qr-code.png"
@@ -9,24 +9,10 @@ function App() {
         />
 
         <div className="flex flex-col gap-4 px-4">
-          <h1
-            style={{
-              fontSize: "22px",
-              lineHeight: "120%",
-              letterSpacing: "0px",
-            }}
-            className="text-slate-900 font-bold text-center"
-          >
+          <h1 className="text-slate-900 font-bold text-center text-[22px] leading-[120%] tracking-normal">
             Improve your front-end skills by building projects
           </h1>
-          <p
-            style={{
-              fontSize: "15px",
-              lineHeight: "140%",
-              letterSpacing: "0.2px",
-            }}
-            className="text-xs text-slate-500 text-center"
-          >
+          <p className="text-[15px] leading-[140%] tracking-[0.2px] text-slate-500 text-center">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
